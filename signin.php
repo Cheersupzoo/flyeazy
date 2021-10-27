56859277a116acaa169edc6240d8b313581bdc3d
vasin <6222772038@g.siit.tu.ac.th>
2021-10-27 16:00:48 +0700

    add checkbox

--- signin.php
+++ signin.php
@@ -26,11 +26,24 @@
 
 <body>
     <div class="mb-3">
-        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Email">
     </div>
     <div class="mb-3">
-        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Password">
     </div>
+
+    <div class="row">
+    </div class="col">
+    <div class="checkbox mb-3">
+        <label>
+            <input type="checkbox" value="remember-me"> Remember me
+        </label>
+    </div>
+    <div class="col">
+        <label for="formforgetlabel" class="form-label">Forgot Password?</label>
+    </div>
+    </div>
+
 </body>
 
 </html>
