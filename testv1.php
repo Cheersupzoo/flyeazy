827bbd658562b58c3b352c82728365d1df928beb
vasin <6222772038@g.siit.tu.ac.th>
2021-10-27 21:33:06 +0700

    add test file

---
+++ testv1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>FlyEazy</title>
+    <?php
+    include 'head.php'
+    ?>
+</head>
+
+<body>
+    <div class="mb-3">
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Email">
+    </div>
+    <div class="mb-3">
+        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Password">
+    </div>
+
+    <div class="row">
+    </div class="col">
+    <div class="checkbox">
+        <label>
+            <input type="checkbox" value="remember-me"> Remember me
+        </label>
+    </div>
+    <div class="col">
+        <label for="formforgetlabel" class="form-label">Forgot Password?</label>
+    </div>
+    </div>
+</body>
+
+</html>
