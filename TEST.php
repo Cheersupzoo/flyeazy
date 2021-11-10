41aa7f628acec54814e1a66628f26a7aa80c3897
vasin <6222772038@g.siit.tu.ac.th>
2021-11-10 23:24:10 +0700

    Complete echo

--- TEST.php
+++ TEST.php
@@ -3,17 +3,22 @@
 
     <body>
         <?php
-       $pdo = require 'connect.php';
-       $username =$_POST["email"];
-       $password =$_POST["password"];
-    
-        $sql = 'SELECT * FROM "User" as u WHERE user."Email" = \''.$username.'\' and u."Password" = \'' . $password .'\'';
+        $pdo = require 'connect.php';
+        $username = $_POST["email"];
+        $password = $_POST["password"];
 
-        if ($username == u."Email" && $password == u."Password") 
-        {echo "Welcome $username!";}
-        else 
-        {echo "INVALID";}
+        $sql = 'SELECT * FROM "User" as u WHERE u."Email" = \''.$username.'\' and u."Password" = \'' . $password .'\'';
+
+        $statement = $pdo->query($sql);
+        $testers = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($testers) {
+            echo "Login Sucessfully";
+        } else {
+            echo "Wrong Username or Password";
+        }
         ?>
+
     </body>
 
     </html>
