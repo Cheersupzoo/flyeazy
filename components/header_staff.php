34f0461ee6c0f6b260c9d010c674b3a02dead5e2
Cheersupzoo <cheersupzoo@gmail.com>
2021-11-19 20:19:53 +0700

    header staff remove booking list

--- components/header_staff.php
+++ components/header_staff.php
@@ -38,7 +38,6 @@ if ($valid) {
     echo <<<HTML
                             </button>
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a class="dropdown-item" href="./booking_list.php">Booking List</a></li>
                                 <li><a class="dropdown-item" href="./signout.php">Sign Out</a></li>
                             </ul>
                         </div>
